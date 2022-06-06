1ad514ca827667a6b7ced87837055d0fb2f78d27
zhshch2002 <zhshch@athorx.com>
2022-06-06 21:27:19 +0800

    Update nexttrace.rb

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,9 +1,9 @@
 class Nexttrace < Formula
   desc "一款开源的可视化路由跟踪CLI工具"
   homepage ""
-  version "v0.1.6-beta.3"
+  version "v0.1.6-beta.4"
   url "https://github.com/xgadget-lab/nexttrace/archive/refs/tags/#{version}.tar.gz"
-  sha256 "5e468c72305bee77487e1927dd928fee5c96a99cfc9afb1e08e2eb813b5a416b"
+  sha256 "cc4bd5fdeac539e2a02e9ea04718f78e007110fb98f7655739cedd9716068293"
   license "GPL-3.0"
 
   depends_on "go" => :build
