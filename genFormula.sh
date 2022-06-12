7244442e52987c3e0794887364e6a5a481d2a7b4
sjlleo <i@leo.moe>
2022-06-12 21:35:54 +0800

    remove: dev channel
    If you want to join Dev Channel, Please use nexttrace-enhanced

--- genFormula.sh
+++ genFormula.sh
@@ -22,26 +22,3 @@ class Nexttrace < Formula
     end
   end
 EOF
-url="https://github.com/xgadget-lab/nexttrace/archive/refs/heads/main.zip"
-sha256="$(curl -sL ${url} | sha256sum | cut -f1 -d' ')"
-version_withoutquo=${sha256:1:8}
-cat >Formula/nexttrace-dev.rb <<EOF
-class NexttraceDev < Formula
-    desc "An open source visual route tracking CLI tool / Dev Channel"
-    homepage "https://trace.ac"
-    version "${version_withoutquo}"
-    url "${url}"
-    sha256 "${sha256}"
-    license "GPL-3.0"
-
-    depends_on "go" => :build
-  
-    def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=${version_withoutquo}' -s -w")
-    end
-  
-    test do
-      assert_match "NextTrace", shell_output("#{bin}/nexttrace-dev -V")
-    end
-  end
-EOF
