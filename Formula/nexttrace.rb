cb8d752293a22fb2a0fb66dc2df23b9e7df4849b
sjlleo <i@leo.moe>
2022-06-18 13:04:47 +0000

    Publish a new version with Formula

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,15 +1,15 @@
 class Nexttrace < Formula
     desc "An open source visual route tracking CLI tool"
     homepage "https://trace.ac"
-    version "v0.1.9"
-    url "https://github.com/xgadget-lab/nexttrace/archive/refs/tags/v0.1.9.tar.gz"
-    sha256 "fc1232f9fcaff652590b3c6cbea80d475c6044de55ce649870b3914bebebec93"
+    version "v0.1.10"
+    url "https://github.com/xgadget-lab/nexttrace/archive/refs/tags/v0.1.10.tar.gz"
+    sha256 "59d43b768a7bcc1af84be7092ce8b2c4a4de007add61c434ac8e37e980dcb87c"
     license "GPL-3.0"
 
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.9' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v0.1.10' -s -w")
     end
   
     test do
