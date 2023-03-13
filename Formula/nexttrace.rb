b521fa0db2a1a35da5ac50d6acd9b9fbe27c4322
sjlleo <i@leo.moe>
2023-03-13 11:43:14 +0000

    Publish a new version with Formula

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -3,7 +3,7 @@ class Nexttrace < Formula
     homepage "https://trace.ac"
     version "v1.1.3"
     url "https://github.com/xgadget-lab/nexttrace/archive/refs/tags/v1.1.3.tar.gz"
-    sha256 "72d8bd43dc0ec41750d3c764edec5b22a54331c4491fd3f4593362ffe3f139fb"
+    sha256 "638b71f9ee735e9129c3712ac0c0777669e272b56d2317f2f331013a20dc4ddb"
     license "GPL-3.0"
 
     depends_on "go" => :build
