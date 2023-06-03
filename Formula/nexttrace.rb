b218e046cbea91aec37a2ca2ed016730265e5097
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-06-03 21:48:28 +0800

    Update genFormula.sh
    Update genFormula.sh nexttrace.rb

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -1,17 +1,17 @@
 class Nexttrace < Formula
     desc "An open source visual route tracking CLI tool"
-    homepage "https://trace.ac"
-    version null
-    url "https://github.com/xgadget-lab/nexttrace/archive/refs/tags/ul.tar.gz"
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    homepage "https://mtr.moe"
+    version "v1.1.7"
+    url "https://github.com/xgadget-lab/nexttrace/archive/refs/tags/v1.1.7.tar.gz"
+    sha256 "58ead840cca5c067f6a5436265025c82604866b124d2462871d56abd0328d4db"
     license "GPL-3.0"
 
     depends_on "go" => :build
-  
+
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=ul' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v1.1.7' -s -w")
     end
-  
+
     test do
       assert_match "NextTrace", shell_output("#{bin}/nexttrace -V")
     end
