71328d9fccbdbcfd6c9f5b83408bffb19c9af98c
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-06-04 20:28:05 +0800

    Update nexttrace.rb

--- Formula/nexttrace.rb
+++ Formula/nexttrace.rb
@@ -9,7 +9,7 @@ class Nexttrace < Formula
     depends_on "go" => :build
   
     def install
-      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/printer.version=v1.1.7-1' -s -w")
+      system "go", "build", *std_go_args(ldflags: "-X 'github.com/xgadget-lab/nexttrace/config.Version=v1.1.7-1' -s -w")
     end
   
     test do
